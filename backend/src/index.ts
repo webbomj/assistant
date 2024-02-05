5c9deddaad14461505f1789ee7ae3cc75dc31408
webbomj <nethack@mail.ru>
2024-02-05 14:32:17 +0300

    chore: add express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log('HELLO')
+import express from 'express'
+
+const expressApp = express()
+expressApp.get('/', (req, res) => {
+  res.send('hello world')
+})
+expressApp.listen(3000, () => {
+  console.info('Listening at http://localhost:3000')
+})
