60baa8b272dba2b4cfd2beff9dfd50e1398537e5
webbomj <nethack@mail.ru>
2024-02-06 14:58:53 +0300

    chore: add pre-commit hook

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
 
+const s = '4'
+
 function App() {
   return (
     <TrpcProvider>
