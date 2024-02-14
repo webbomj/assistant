590011ffd423697c0ea21c1070db7a0268bb1b83
webbomj <nethack@mail.ru>
2024-02-14 14:36:56 +0300

    feat: add infinity scroll to all ideas

--- webapp/src/components/Layout/index.tsx
+++ webapp/src/components/Layout/index.tsx
@@ -1,3 +1,4 @@
+import { createRef } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import { useMe } from '../../lib/ctx'
 import {
@@ -9,6 +10,8 @@ import {
   getSignUpRoute,
 } from '../../lib/routes'
 import css from './index.module.scss'
+
+export const layoutContentElRef = createRef<HTMLDivElement>()
 
 export const Layout = () => {
   const me = useMe()
@@ -56,7 +59,7 @@ export const Layout = () => {
           )}
         </ul>
       </div>
-      <div className={css.content}>
+      <div className={css.content} ref={layoutContentElRef}>
         <Outlet />
       </div>
     </div>
--- webapp/src/pages/ideas/AllIdeasPage/index.tsx
+++ webapp/src/pages/ideas/AllIdeasPage/index.tsx
@@ -1,5 +1,7 @@
+import InfiniteScroll from 'react-infinite-scroller'
 import { Link } from 'react-router-dom'
 import { Alert } from '../../../components/Alert'
+import { layoutContentElRef } from '../../../components/Layout'
 import { Segment } from '../../../components/Segment'
 import { getViewIdeaRoute } from '../../../lib/routes'
 import { trpc } from '../../../lib/trpc'
@@ -8,9 +10,7 @@ import css from './index.module.scss'
 export const AllIdeasPage = () => {
   const { data, error, isLoading, isFetching, isError, hasNextPage, fetchNextPage, isFetchingNextPage, isRefetching } =
     trpc.getIdeas.useInfiniteQuery(
-      {
-        limit: 2,
-      },
+      {},
       {
         getNextPageParam: (lastPage) => {
           return lastPage.nextCursor
@@ -35,33 +35,38 @@ export const AllIdeasPage = () => {
         <Alert color="red">{error}</Alert>
       ) : (
         <div className={css.ideas}>
-          {data.pages
-            .flatMap((page) => page.ideas)
-            .map((idea) => (
-              <div className={css.idea} key={idea.nick}>
-                <Segment
-                  size={2}
-                  title={
-                    <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
-                      {idea.name}
-                    </Link>
-                  }
-                  description={idea.description}
-                />
+          <InfiniteScroll
+            threshold={150}
+            loadMore={() => {
+              if (!isFetchingNextPage && hasNextPage) {
+                void fetchNextPage()
+              }
+            }}
+            hasMore={hasNextPage}
+            loader={
+              <div className={css.more} key="loader">
+                Loading...
               </div>
-            ))}
-          <div className={css.more}>
-            {hasNextPage && !isFetchingNextPage && (
-              <button
-                onClick={() => {
-                  void fetchNextPage()
-                }}
-              >
-                Load more
-              </button>
-            )}
-            {isFetchingNextPage && <span>Loading...</span>}
-          </div>
+            }
+            getScrollParent={() => layoutContentElRef.current}
+            useWindow={(layoutContentElRef.current && getComputedStyle(layoutContentElRef.current).overflow) !== 'auto'}
+          >
+            {data.pages
+              .flatMap((page) => page.ideas)
+              .map((idea) => (
+                <div className={css.idea} key={idea.nick}>
+                  <Segment
+                    size={2}
+                    title={
+                      <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
+                        {idea.name}
+                      </Link>
+                    }
+                    description={idea.description}
+                  />
+                </div>
+              ))}
+          </InfiniteScroll>
         </div>
       )}
     </Segment>
