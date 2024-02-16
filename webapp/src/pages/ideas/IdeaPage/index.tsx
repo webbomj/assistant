a1e7bcf0e3dc62af897286a9b80d0a091d32a870
webbomj <nethack@mail.ru>
2024-02-16 10:43:20 +0300

    feat: add 404 image

--- webapp/src/pages/ideas/IdeaPage/index.tsx
+++ webapp/src/pages/ideas/IdeaPage/index.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom'
 import { Alert } from '../../../components/Alert'
 import { Button, LinkButton } from '../../../components/Button'
 import { FormItems } from '../../../components/FormItems'
+import { Icon } from '../../../components/Icon'
 import { Segment } from '../../../components/Segment'
 import { useForm } from '../../../lib/form'
 import { withPageWrapper } from '../../../lib/pageWrapper'
@@ -40,7 +41,7 @@ const LikeButton = ({ idea }: { idea: NonNullable<TrpcRouterOutput['getIdea']['i
         void setIdeaLike.mutateAsync({ ideaId: idea.id, isLikedByMe: !idea.isLikedByMe })
       }}
     >
-      {idea.isLikedByMe ? 'Unlike' : 'Like'}
+      <Icon name={idea.isLikedByMe ? 'likeFilled' : 'likeEmpty'} size={32} className={css.likeIcon} />
     </button>
   )
 }
--- webapp/src/pages/other/NotFoundPage/index.tsx
+++ webapp/src/pages/other/NotFoundPage/index.tsx
@@ -1,4 +1,6 @@
+import image404 from '../../../assets/404.png'
 import { ErrorPageComponent } from '../../../components/ErrorPageComponent'
+import css from './index.module.scss'
 
 export const NotFoundPage = ({
   title = 'Not Found',
@@ -6,4 +8,8 @@ export const NotFoundPage = ({
 }: {
   title?: string
   message?: string
-}) => <ErrorPageComponent title={title} message={message} />
+}) => (
+  <ErrorPageComponent title={title} message={message}>
+    <img src={image404} className={css.image} alt="" width="800" height="600" />
+  </ErrorPageComponent>
+)
